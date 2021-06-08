eaee0fe786dd5acc8e5a0d23b194215aa9bcd995
Marcy Montross <mmontros@nerdery.com>
2021-06-08 13:10:55 -0500

    new repo

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,1 +1,10 @@
-// This file is empty, but some people were reporting that it would not start unless they had an empty file. So here it is! You can delete the comment. Or replace it with your favourite shania twain lyrics.
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [],
+}
