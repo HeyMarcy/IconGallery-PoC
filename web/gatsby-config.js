7b326abae44152552e6112ff2682606aef5b07ca
Marcy Montross <mmontros@nerdery.com>
2021-06-09 20:13:50 -0500

    enviroment

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -1,10 +1,28 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.com/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
-}
+  siteMetadata: {
+    title: `Icon Gallery`,
+    description: `Icon Gallery Proof of Concept`,
+    author: `Marcy`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [`Merriweather\:700`, `Lato\:400,700`, `Literata\:900`],
+        display: 'swap',
+      },
+    },
+    `gatsby-plugin-image`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: false,
+      },
+    },
+
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+};
